2a47f4554ef324fc5400fe09d18ad2e76eaf7511
Adil2602 <mrlimon2017@gmail.com>
2023-12-09 23:47:48 +0600

    register and login

--- config/urls.py
+++ config/urls.py
@@ -19,12 +19,13 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.product.urls'))
+    path('account/', include('apps.account.urls')),
+    path('', include('apps.product.urls')),
 ]
 
-from django.conf import settings
-from django.conf.urls.static import static
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# from django.conf import settings
+# from django.conf.urls.static import static
+#
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
