a3a44c5268ca1c5d921799d59f7ad2cc01cf9b45
Sandora <s0vetbek0v.muhamed@gmail.com>
2023-12-13 21:45:50 +0600

    amin cart 13.12.2023 21:45

--- apps/basket/admin.py
+++ apps/basket/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.db.models import Sum
+from django.db.models.functions import Coalesce
+from ..basket.models import Cart, CartItem
+from django.db.models import F
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 1  # Количество дополнительных полей для отображения
 
-# Register your models here.
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',
+        'item_count',
+        'total_price',
+    ]
+    inlines = [CartItemInline]
+
+    def item_count(self, obj):
+        return obj.cartitem_set.aggregate(Sum('quantity'))['quantity__sum']
+    item_count.short_description = 'Item Count'
+
+    def total_price(self, obj):
+        return obj.cartitem_set.aggregate(
+            total_price=Coalesce(Sum(F('quantity') * F('product__price')), 0)
+        )['total_price']
+
+    total_price.short_description = 'Total Price'
+
+
--- apps/basket/models.py
+++ apps/basket/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from ..account.models import User
+from ..product.models import Product
 # Create your models here.
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    product = models.ManyToManyField(Product)
+    def __str__(self):
+        return f'{self.user} - {self.product} '
+
+    class Meta:
+        verbose_name = 'Корзина'
+        verbose_name_plural = 'Корзина'
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+
