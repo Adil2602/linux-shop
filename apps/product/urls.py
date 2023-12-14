8b8f0001a110f3d3239e2704e5a07443b72718f4
Adil2602 <mrlimon2017@gmail.com>
2023-12-14 17:31:58 +0600

    serializers

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-
-from .views import ProductList
+from .views import *
 
 urlpatterns = [
-    path('product/', ProductList.as_view(), name='product')
+    path('product/',
+         ProductList.as_view(),
+         name='product'),
+
+    path('api/list/',
+         ProductListApiView.as_view(),
+         name='ListApi'),
+
+    path('api/retrieve/',
+         ProductRetrieveApiView.as_view(),
+         name='RetrieveApi'),
 ]
--- apps/product/views.py
+++ apps/product/views.py
@@ -2,10 +2,24 @@ from django.db import models
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Product
+from rest_framework import generics
+from rest_framework import views
+from .serializers import ProductSerializer
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 class ProductList(ListView):
     model = Product
     template_name = 'product.html'
     context_object_name = 'products'
+    queryset = Product.objects.all()
+
+class ProductListApiView(generics.ListAPIView):
+    serializer_class = ProductSerializer
+    context_object_name = 'products'
+    queryset = Product.objects.all()
+    # permission_classes = IsAuthenticated
+
+class ProductRetrieveApiView(generics.RetrieveAPIView):
+    serializer_class = ProductSerializer
     queryset = Product.objects.all()
