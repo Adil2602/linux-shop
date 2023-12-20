725e1fe5d8558b9c1f49204139dfb79fb2764151
Sandora <s0vetbek0v.muhamed@gmail.com>
2023-12-20 22:40:02 +0600

    Api ViewSet for User

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 
 from .views import *
+from rest_framework.routers import SimpleRouter, DefaultRouter
+
+router = DefaultRouter()
+router.register('user', UserApiViewSet, basename='user')
 
 urlpatterns = [
+    path('api/v1/', include(router.urls)),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', UserLogout, name='logout'),
--- apps/account/views.py
+++ apps/account/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, FormView, CreateView
+from rest_framework.viewsets import GenericViewSet
+
 from .forms import RegisterForm, LoginForm
 from django.urls import reverse_lazy
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
+from rest_framework import viewsets, mixins
 from django.template.context_processors import request
+
+from .models import User
+from .serializers import UserSerializer
+
+
 class RegisterView(CreateView):
-    template_name ='register.html'
+    template_name = 'register.html'
     form_class = RegisterForm
     success_url = reverse_lazy('login')
+
 
 class LoginView(FormView):
     template_name = 'login.html'
@@ -20,7 +29,7 @@ class LoginView(FormView):
         email = data['email']
         password = data['password1']
         user = authenticate(username=username, email=email,
-        password=password)
+                            password=password)
         if user is not None:
             if user.is_active:
                 login(self.request, user)
@@ -34,3 +43,16 @@ def UserLogout(request):
     if request.user.is_authenticated:
         logout(request)
         return redirect('category')
+
+
+# API
+
+
+class UserApiViewSet(mixins.CreateModelMixin,
+                     mixins.RetrieveModelMixin,
+                     mixins.UpdateModelMixin,
+                     # mixins.DestroyModelMixin,
+                     mixins.ListModelMixin,
+                     GenericViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
